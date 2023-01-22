9cee015b2b824a0264b2c3d26f562215ed612258
branjthacker <branjthacker@gmail.com>
2023-01-22 14:38:08 -0800

    Yummy Yummy Vesc Calibration App

--- applications/app_adc.c
+++ applications/app_adc.c
@@ -70,6 +70,16 @@ static volatile bool adc_detached = false;
 static volatile bool buttons_detached = false;
 static volatile bool rev_override = false;
 static volatile bool cc_override = false;
+//variables for calibration
+static volatile int next = 0; //keeps track of next state
+static volatile int state = 0; //keeps track of current state
+static volatile int phase = 1; //keeps track of calibration phase ***work on incorperating this***
+static volatile float time_at_speed = 0; //keeps track of the time we are at a given speed
+static volatile float pwr_array[90] = {0.0}; //records powers in phase 2 to be read in phase 3
+static volatile bool collect = false; //flag for PI, states if data should be read or not
+static volatile float current_speed = 0.0;  // what it says on the tin
+static volatile float avg_pwr_at_speed = 0.0; // what it says on the tin
+
 
 void app_adc_configure(adc_config *conf) {
 	if (!buttons_detached && (((conf->buttons >> 0) & 1) || CTRL_USES_BUTTON(conf->ctrl_type))) {
@@ -154,10 +164,184 @@ void app_adc_cc_override(bool state){
 	cc_override = state;
 }
 
-float calc_efficient_power()
+void update_speed_time(){
+	//will tick up the amount of time the car remains at a given speed, resets to 0 once a speed 
+	//change is detected
+	//NOTE: MAY NEED A BIT OF LEEWAY FOR CURRENT SPEED EQUALITY 
+	if (current_speed - 0.05 <= mc_interface_get_speed() <= current_speed + 0.05){
+		time_at_speed += 0.0001;
+	}
+	else{
+		current_speed = mc_interface_get_speed();
+		time_at_speed = 0.0;
+	}
+}
+
+void update_speed_pwr(pwr){
+	//averages the power usage while speed is constant, the variable speed_pwr can then be used to 
+	//update the appropriate variables
+	//AGAIN MAY NEED SOME LEEWAY IN EQUALITY CUZ FLOATS
+	if (current_speed - 0.05 <= mc_interface_get_speed() <= current_speed + 0.05){
+		//update power with rolling average
+		avg_pwr_at_speed = ((avg_pwr_at_speed * 1000 * time_at_speed) + pwr) / (time_at_speed * 1000);
+	}
+	else{
+		//if 1st time at speed start the average off at the current power
+		avg_pwr_at_speed = pwr;
+	}
+}
+
+float burn_to_speed(int speed, float pwr){
+	//Given a speed in m/s, seeks to get to that speed and maintain that power
+	//need a system to average the below values and record them int a table.
+	if (mc_interface_get_speed() < speed / 10 && pwr >= 0.5){
+		return 1;
+	}
+	return 0;
+}
+
+float burn_at_power(int state, float pwr){
+	//Given the state and the power array, lookup power for that state and drive at that power
+	if (pwr >= 0.5){
+		return pwr_array[state];
+	}
+	return 0;
+}
+
+float standby_no_data(float turbo){
+	//function is called if we are in standby 
+	if ( turbo >= 0.5 ){
+		return 1.0;
+	}
+
+	else{
+		return 0.0;
+	}
+	
+}
+
+void change_state(float turbo, float pwr, float time_at_speed){
+	//given the current state, next state, if pwr is high, and how long we've been at a speed
+	//we select the next state, need a system to discard data and go to appropriate place if turbo
+	switch (state)
+	{
+	case 0:
+		//if we are in standby see if there is pwr, if so check phase and return data accordingly
+		collect = false; //whenever we get to standby, stop collecting data, whenever we leave begin
+		if (pwr >= 0.5 && mc_interface_get_speed() == 0){
+			collect = true;
+			state = next;
+		}
+		break;
+
+	case 10:
+		//check to see if we're at max speed then go to special coast state i forgor to code
+		if (time_at_speed > 2.0){
+			next = 20;
+			state = 11;
+		}
+		break;
+	
+	case 11:
+		//coast to 0 then go to standby for phase 2
+		if (mc_interface_get_speed() <= 0.5){
+			state = 0;
+		}
+		break;
+	
+	//any phase 2 case where next state is an increment of 5 
+	case 20: case 25: case 30: case 35: case 40: case 45: case 50: case 55: 
+
+		if (time_at_speed > 2.0){
+				next = state + 5; 
+				pwr_array[state] = avg_pwr_at_speed; //update the pwr array with the avg power 
+				state = 0; //resets state to standby so we jump to the next state on the next loop
+			}	
+		break;
+
+
+	//any phase 2 case where next state is an increment of 2
+	case 120: case 125: case 130: case 135: case 140: case 145: case 150: case 155:
+
+		if (time_at_speed > 2.0){
+				next = state + 5; 
+				state = 0;
+			}	
+		break;
+	
+	//any phase 2 case where next state is an increment of 2
+	case 60: case 62: case 64: case 66: case 68: case 70: case 72: case 74: case 78: case 80:
+	case 82: case 84: case 86: case 88: case 90:
+
+		if (time_at_speed > 2.0){
+				next = state + 2; 
+				pwr_array[state] = avg_pwr_at_speed; //update the pwr array with the avg power 
+				state = 0;
+			}	
+		break;
+
+	
+	//any phase 3 case where next state is an increment of 2
+	case 160: case 162: case 164: case 166: case 168: case 170: case 172: case 174: case 178: 
+	case 180: case 182: case 184: case 186: case 188: case 190:
+	
+		if (time_at_speed > 2.0){
+				next = state + 2; 
+				state =  0;
+			}	
+		break;
+	
+	default:
+		//if we aren't in a state treat as though we are in standby
+		next = 10;
+		state = 0;
+		break;
+
+	}
+}
+
+float execute_calibration_step(float turbo, float pwr)
 {
-	//float rpm = mc_interface_get_rpm();
-	return 0.5;
+	//given the state we are in, this function will return the appropriate motor power
+
+	switch (state)
+	{
+	case 0:
+		//standby allow the car to be positioned for the next state
+		return standby_no_data(turbo);
+		break;
+	
+	case 10:
+		//max power burn, return 1.0
+		return 1.0;
+		break;
+	
+	case 11:
+		//coast collection state
+		return 0.0;
+		break;
+	
+	//next group of states is phase 2 where we go to each speed and record the power in pwr array
+	case 20: case 25: case 30: case 35: case 40: case 45: case 50: case 55: case 60: 
+	case 62: case 64: case 66: case 68: case 70: case 72: case 74: case 78: case 80:
+	case 82: case 84: case 86: case 88: case 90:
+
+		return burn_to_speed(state, pwr);
+		break;
+	
+	//next group of states is phase 3 where we go through each power in the power array
+	case 120: case 125: case 130: case 135: case 140: case 145: case 150: case 155: case 160: 
+	case 162: case 164: case 166: case 168: case 170: case 172: case 174: case 178: case 180:
+	case 182: case 184: case 186: case 188: case 190:
+
+		return burn_at_power(state - 100, pwr);
+		break;
+
+	default:
+		//if we aren't in a state treat as though we are in standby
+		return standby_no_data(turbo);
+		break;
+	} 
 }	
 
 static THD_FUNCTION(adc_thread, arg) {
@@ -296,19 +480,17 @@ static THD_FUNCTION(adc_thread, arg) {
 		// Apply throttle curve (COME BACK TO)
 		// pwr = utils_throttle_curve(pwr, config.throttle_exp, config.throttle_exp_brake, config.throttle_exp_mode);
 
-		if ( turbo >= 0.5 )
-		{
-			pwr = 1.0;
-		}
-		else if ( pwr >= 0.5 )
-		{
-			pwr = calc_efficient_power();
-		}
-		else
-		{
-			pwr = 0;
-		}
+		//need here: time at speed tracking, speed power averaging and 
+		// storing -> implimented in a few new functions
+		
+		//update the state and then find the appropriate power. That's it
+		update_speed_time();
+		change_state(turbo, pwr, time_at_speed);
+		pwr = execute_calibration_step(turbo, pwr);
+		update_speed_pwr(); //keeps the average power at speed for storage in array
 
+
+		
 		// commands_printf("POWER: %.2f\n", pwr);
 
 
